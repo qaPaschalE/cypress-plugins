ee30fe99683a73d0e9b79df8652375eb37b83a38
Paschal Enyimiri <paschal.enyimiri@indicina.co>
2025-04-09 17:14:55 +0100

    fixes on test

--- cypress-ms-teams-reporter/cypress.config.ts
+++ cypress-ms-teams-reporter/cypress.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "cypress";
 import mochawesome from "cypress-mochawesome-reporter/plugin";
-import { sendTeamsReport } from "../cypress-ms-teams-reporter";
+import { sendTeamsReport } from "../cypress-ms-teams-reporter/src/index.js";
+
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
   await mochawesome(on);
-
-  on("task", {
-    sendTeamsReport(options) {
-      return sendTeamsReport(options);
-    },
-  });
 
   return config;
 }
